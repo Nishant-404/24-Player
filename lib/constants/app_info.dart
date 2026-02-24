74440f869b5be1c2c9551d78f081705acbef5ce6
Nishant Sharma <nishant.flyy@gmail.com>
2026-02-25 00:10:03 +0530

    Initial commit: 24-Player Base (v1.0.0)

---
+++ lib/constants/app_info.dart
@@ -0,0 +1,21 @@
+/// App version and info constants
+/// Update version here only - all other files will reference this
+class AppInfo {
+  static const String version = '4.0.0';
+  static const String buildNumber = '101';
+  static const String fullVersion = '$version+$buildNumber';
+  
+  
+  static const String appName = 'SpotiFLAC';
+  static const String copyright = '© 2026 SpotiFLAC';
+  
+  static const String mobileAuthor = 'zarzet';
+  static const String originalAuthor = 'afkarxyz';
+  
+  static const String githubRepo = 'zarzet/SpotiFLAC-Mobile';
+  static const String githubUrl = 'https://github.com/$githubRepo';
+  static const String originalGithubUrl = 'https://github.com/afkarxyz/SpotiFLAC';
+  
+  static const String kofiUrl = 'https://ko-fi.com/zarzet';
+  static const String githubSponsorsUrl = 'https://github.com/sponsors/zarzet/';
+}
